357114c8c746e0bd5a1046eaf13fba5bfdedddd6
Arunkumar2602 <arunrockz01@gmail.com>
2021-07-22 10:23:10 -0400

    arun push

---
+++ src/org/sample/DemoClass.java
@@ -0,0 +1,16 @@
+package org.sample;
+
+public class DemoClass 
+{
+
+	private void demo1() {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+	public static void main(String[] args) {
+		DemoClass classs = new DemoClass();
+		classs.demo1();
+				
+	}
+
+}
